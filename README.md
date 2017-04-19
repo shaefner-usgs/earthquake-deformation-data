a9be03db0bfcff30bb75e5769b7a884105799f5e
Scott Haefner <shaefner@usgs.gov>
2017-04-19 11:50:35 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# earthquake-deformation-data
+
+USGS Deformation data: Fault Creep, Borehole Strain, and Tiltmeter Monitoring Measurements
+
+https://earthquake.usgs.gov/monitoring/deformation/data
---
+++ src/conf/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+date_default_timezone_set('UTC');
+
+$CONFIG_INI_FILE = dirname(__FILE__) . '/config.ini';
+if (!file_exists($CONFIG_INI_FILE)) {
+  trigger_error('Application not configured. Run pre-install script.');
+  exit(-1);
+}
+
+$CONFIG = parse_ini_file($CONFIG_INI_FILE);
+
+$APP_DIR = $CONFIG['APP_DIR'];
+$DATA_DIR = $CONFIG['DATA_DIR'];
+$DB_DSN = $CONFIG['DB_DSN'];
+$DB_USER = $CONFIG['DB_USER'];
+$DB_PASS = $CONFIG['DB_PASS'];
+$MOUNT_PATH = $CONFIG['MOUNT_PATH'];
