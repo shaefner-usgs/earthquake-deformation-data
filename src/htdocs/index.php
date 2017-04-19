306db2084f7734ec9f27d046c4490a538c63f2e4
Scott Haefner <shaefner@usgs.gov>
2017-04-19 16:23:15 -0700

    add text

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,25 +1,19 @@
 <?php
 
 if (!isset($TEMPLATE)) {
-  // template functions
-  include_once 'functions.inc.php';
-
-  // defines the $CONFIG hash of configuration variables
-  include_once '../conf/config.inc.php';
-
-  $TITLE = 'Hazdev Project Skeleton v{{VERSION}}';
-  $HEAD = '
-    <link rel="stylesheet" href="/lib/leaflet-0.7.7/leaflet.css" />
-    <link rel="stylesheet" href="css/index.css"/>
-  ';
-  $FOOT = '
-    <script src="/lib/leaflet-0.7.7/leaflet.js"></script>
-    <script src="js/index.js"></script>
-  ';
+  $TITLE = 'Fault Creep, Borehole Strain, and Tiltmeter Monitoring Measurements';
+  $HEAD = '';
+  $FOOT = '';
 
   include 'template.inc.php';
 }
 
 ?>
 
-<div class="map"></div>
+<p>The US Geological Survey maintains a variety of fault and volcano monitoring sites around the western United States. Instruments at these sites include strainmeters, tiltmeters and creepmeters, as well as other environmental parameters such as temperature and barometric pressure.</p>
+
+<p>The data are collected and monitored to help understand how, when, and why large earthquakes, fault slip and volcanic activity occur. The measurements provide a near real-time record of the related crustal deformation before, during and after events. The goal is to better understand these natural processes, and use these data to reduce the earthquake and volcanic hazards associated with them.</p>
+
+<p>This web site provides <a href="plots.php">data plots</a> and <a href="download.php">data downloads</a> for many instruments that are concentrated in areas where large earthquakes are likely to occur in California and areas of known volcanic activity (Long Valley, CA). In particular, the USGS has concentrated instrumentation efforts in the San Francisco Bay Area, near San Juan Bautista and Parkfield, and the Long Valley, CA and Southern California regions.</p>
+
+<p class="disclaimer">The plots and data on this site are generated automatically and are not reviewed. They should not be used for engineering, legal, or any other critical applications.</p>
