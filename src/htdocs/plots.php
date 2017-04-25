3d9191f10a36215b78013a4d1f99a3fb42ec412a
Scott Haefner <shaefner@usgs.gov>
2017-04-25 08:06:56 -0700

    add text / links for each region

--- src/htdocs/plots.php
+++ src/htdocs/plots.php
@@ -10,16 +10,174 @@ if (!isset($TEMPLATE)) {
   $TITLE = 'Data Plots';
   $HEAD = '
     <link rel="stylesheet" href="/lib/leaflet-0.7.7/leaflet.css" />
-    <link rel="stylesheet" href="css/plots.css"/>
+    <link rel="stylesheet" href="css/plots.css" />
   ';
   $FOOT = '
+    <script>
+      var MOUNT_PATH = "' . $CONFIG['MOUNT_PATH'] . '";
+    </script>
     <script src="/lib/leaflet-0.7.7/leaflet.js"></script>
     <script src="js/plots.js"></script>
   ';
+  $NAVIGATION = true;
 
   include 'template.inc.php';
 }
 
 ?>
 
+<h2>Map of Instrument Sites</h2>
+
 <div class="map"></div>
+
+<h2>Long Valley</h2>
+
+<p>Long Valley Caldera is located 20 km south of Mono Lake along the east side of the Sierra Nevada in east-central California. This area of eastern California has produced numerous volcanic eruptions over the past 3 million years including the massive, caldera-forming eruption 760,000 years ago. The most recent eruptions occurred just 500&ndash;600 years ago from vents along the Mono Crater-Inyo crater volcanic chain, which extends south from Mono Lake into the western part of Long Valley caldera.</p>
+
+<p>A strong earthquake swarm in May of 1980, which included four magnitude 6 earthquakes near the southern margin of the caldera and increased uplift and extension of the Long Valley Caldera, marked the onset of the latest a period of activity in the caldera. This activity continues today and includes recurring earthquake swarms and continued dome-shaped uplift of the central section of the caldera accompanied by changes in thermal springs and gas emissions.</p>
+
+<p>In addition to crustal deformation measurements, the USGS <a href="https://volcanoes.usgs.gov/observatories/calvo/"> California Volcano Observatory</a> monitors earthquakes, hydrologic conditions, gas discharge, and other geophysical conditions in the Long Valley Caldera region.</p>
+
+<table>
+  <tr>
+    <th>Instrument</th>
+    <th colspan="3">Plots</th>
+  </tr>
+  <tr>
+    <td>Dilatometers</td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/lv_10.png">Past 10 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/lv_30.png">Past 30 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/lv_100.png">Past 100 days</a></td>
+  </tr>
+  <tr>
+    <td>Tiltmeters</td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/lv_tilt_10.png">Past 10 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/lv_tilt_30.png">Past 30 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/lv_tilt_100.png">Past 100 days</a></td>
+  </tr>
+</table>
+
+<h2>Parkfield</h2>
+
+<p>Since the <a href="/research/parkfield/">Parkfield</a> area is a region of fairly frequent, moderate-sized earthquakes, the USGS installed a variety of instruments that measure strain changes and slip. On September 28, 2004 a Magnitude 6.0 earthquake occurred 11 kilometers southeast of the town of Parkfield. The USGS continues to monitor these instruments to document the deformation prior to, during, and after such events. The role that these earthquakes play in the eventual failure of the locked section of the San Andreas fault just to the south is also of great interest.</p>
+
+<table class="tabular">
+  <tr>
+    <th>Instrument</th>
+    <th colspan="6">Plots</th>
+  </tr>
+  <tr>
+    <td>Creepmeters</td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/pkf_10.png">Past 7 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/pkf_30.png">Past 30 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/pkf_100.png">Past 100 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/pkf_730.png">Past 2 years</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/pkf_1200.png">Past 3.2 years</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/pkf_3650.png">Past 10 years</a></td>
+  </tr>
+  <tr>
+    <td>Dilatometers</td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/pkf_10.png">Past 7 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/pkf_30.png">Past 30 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/pkf_100.png">Past 100 days</a></td>
+    <td class="no-data" title="No data">
+      <i class="material-icons">&#xE14B;</i></td>
+    <td class="no-data" title="No data">
+      <i class="material-icons">&#xE14B;</i></td>
+    <td class="no-data" title="No data">
+      <i class="material-icons">&#xE14B;</i></td>
+  </tr>
+</table>
+
+<h2>San Francisco Bay Area</h2>
+
+<p>The San Francisco Bay area has been identified in a <a href="https://pubs.usgs.gov/fs/2015/3009/">recent report</a> as a region where a magnitude 6.7 or greater earthquake is likely to occur in the next 30 years. USGS installed a variety of instruments that measure strain changes and fault slip near the many active earthquake faults in the region.</p>
+
+<table>
+  <tr>
+    <th>Instrument</th>
+    <th colspan="6">Plots</th>
+  </tr>
+  <tr>
+    <td>Creepmeters</td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/creep_10.png">Past 7 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/creep_30.png">Past 30 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/creep_100.png">Past 100 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/creep_730.png">Past 2 years</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/creep_1200.png">Past 3.2 years</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/creep_3650.png">Past 10 years</a></td>
+  </tr>
+  <tr>
+    <td>Dilatometers</td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/sfb_10.png">Past 7 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/sfb_30.png">Past 30 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/sfb_100.png">Past 100 days</a></td>
+    <td class="no-data" title="No data">
+      <i class="material-icons">&#xE14B;</i></td>
+    <td class="no-data" title="No data">
+      <i class="material-icons">&#xE14B;</i></td>
+    <td class="no-data" title="No data">
+      <i class="material-icons">&#xE14B;</i></td>
+  </tr>
+  <tr>
+    <td>DTM Tensor</td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/miniPBO1_10.png">Past 7 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/miniPBO1_30.png">Past 30 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/miniPBO1_100.png">Past 100 days</a></td>
+    <td class="no-data" title="No data">
+      <i class="material-icons">&#xE14B;</i></td>
+    <td class="no-data" title="No data">
+      <i class="material-icons">&#xE14B;</i></td>
+    <td class="no-data" title="No data">
+      <i class="material-icons">&#xE14B;</i></td>
+  </tr>
+  <tr>
+    <td>Tiltmeter</td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/miniPBO_tilt_10.png">Past 7 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/miniPBO_tilt_30.png">Past 30 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/miniPBO_tilt_100.png">Past 100 days</a></td>
+    <td class="no-data" title="No data">
+      <i class="material-icons">&#xE14B;</i></td>
+    <td class="no-data" title="No data">
+      <i class="material-icons">&#xE14B;</i></td>
+    <td class="no-data" title="No data">
+      <i class="material-icons">&#xE14B;</i></td>
+  </tr>
+</table>
+
+<h2>San Juan Bautista</h2>
+
+<p>The San Juan Bautista area covers the junction between the northern end of the creeping section of the San Andreas Fault and the southern section of the (now locked) region of the fault that last ruptured during the great 1906 San Francisco earthquake. The region thus forms a natural laboratory where the processes of stable sliding (creep) and unstable sliding (earthquakes) is closely monitored. Moderate magnitude earthquakes have occurred quite frequently in this region in the past. The largest and most recent event was the 1989 M7.1 Loma Prieta earthquake.</p>
+
+<table>
+  <tr>
+    <th>Instrument</th>
+    <th colspan="6">Plots</th>
+  </tr>
+  <tr>
+    <td>Creepmeters</td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/sjb_10.png">Past 7 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/sjb_30.png">Past 30 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/sjb_100.png">Past 100 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/sjb_730.png">Past 2 years</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/sjb_1200.png">Past 3.2 years</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/Creep/sjb_3650.png">Past 10 years</a></td>
+  </tr>
+</table>
+
+<h2>Southern California</h2>
+
+<p> The Southern California area is subject to thousands of earthquakes each year. In addition to the San Andreas Fault, there are over 200 known faults that can produce magnitude 6 or greater earthquakes. A small network of strainmeters monitors the region in the central transverse ranges of this region.</p>
+
+<table>
+  <tr>
+    <th>Instrument</th>
+    <th colspan="3">Plots</th>
+  </tr>
+  <tr>
+    <td>Dilatometers</td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/scal_10.png">Past 7 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/scal_30.png">Past 30 days</a></td>
+    <td><a href="https://escweb.wr.usgs.gov/share/langbein/Web/USGS_strain/scal_100.png">Past 100 days</a></td>
+  </tr>
+</table>
